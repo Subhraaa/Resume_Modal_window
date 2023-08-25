59b16f817e361d78aa05169b8e9324c6decd0614
Subhra <83825043+Subhraaa@users.noreply.github.com>
2023-08-25 11:22:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,43 +7,8 @@ const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-const openModal = function(){
-    console.log("Button clicked");
-    //to display the modal
-    //to act on a class of HTML, we use classList property of modal class. 
-    modal.classList.remove('hidden')//to add multiple multiple classes to be removed ('hidden', 'btn'..)
-    //('.hidden') is used only for selector and here it is('hidden')
-    overlay.classList.remove('hidden');
 
-    // //we could also change the css value of display property of HIDDEN class 
-    // modal.style.display = 'block';
-}
-
-
-
-//we want to implement a feature of closing the modal window by pressing esc key
-//keyboard event listener is a global event i.e. we can't use it on a single particular html element
-//and global events are listened on whole document so
-document.addEventListener('keydown',function(e){ //so far we never tried to access the "event" object of addEventlistener
-    //e stands for event object passed as parameter
-    if(e.key === 'Escape'){
-     for(let i = 0; i<modal.length; i++){
-        if(!modal[i].classList.contains('hidden'))
-        {//key is a property of keyboard event e which reads the type of key pressed like spacebar, esc key
-        modal[i].classList.add('hidden');
-        overlay[i].classList.add('hidden');
-        }
-}
-}
-});
-//there are three keyboard events i.e. keydown, keypress and keyup
-// keyup - when we lift a key off the Keyboard
-//keydown - as soon as we press down a key
-//keypress - fired continuously press a key
-
-
-//to access the text content of all the  three show modal buttons
-//we could run a for loop because we have used query selectorall instead of query selectorall(it forms a array like structure)
+//to open the modal window by clicking the modal button
 for(let i = 0; i<btnsOpenModal.length; i++){
     btnsOpenModal[i].addEventListener('click',function(){
         modal[i].classList.remove('hidden');
@@ -51,6 +16,7 @@ for(let i = 0; i<btnsOpenModal.length; i++){
     });
 } 
 
+//to close the modal window by clicking on the close button
 for(let i = 0; i<btnCloseModal.length; i++){
     btnCloseModal[i].addEventListener('click',function(){
         modal[i].classList.add('hidden');
@@ -58,11 +24,23 @@ for(let i = 0; i<btnCloseModal.length; i++){
     });
 } 
 
-//we want to able to close the model even if we click on the overlay
-//if we copy the above function for overlay variable as well, code would be longer. we instead create a function closeModal
+//to close the modal window by just clicking on the overlay
 for(let i = 0; i<overlay.length; i++){
     overlay[i].addEventListener('click',function(){
         modal[i].classList.add('hidden');
         overlay[i].classList.add('hidden');
     });
 } 
+
+//to close the modal window by pressing escape key
+document.addEventListener('keydown',function(e){ 
+    if(e.key === 'Escape'){
+     for(let i = 0; i<modal.length; i++){
+        if(!modal[i].classList.contains('hidden'))
+        {
+        modal[i].classList.add('hidden');
+        overlay[i].classList.add('hidden');
+        }
+    }
+}
+});
